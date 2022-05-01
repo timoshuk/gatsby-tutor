b749a66dba96454d8e9219a61fea3c40d7debd4a
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2022-05-01 18:14:56 +0300

    Create two pages

---
+++ src/pages/about.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About page">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium
+        reiciendis hic eum, asperiores facilis quasi ipsum architecto voluptatem
+        nesciunt iure dolores excepturi, repellat ad maiores facere repellendus
+        placeat voluptatibus ipsam!
+      </p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
