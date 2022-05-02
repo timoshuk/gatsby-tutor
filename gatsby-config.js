9c02649b8a18312fc425494767bdc6dd5e15685c
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2022-05-02 17:01:59 +0300

    Add image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     title: `gatsby-tutor`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 
@@ -8,6 +8,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="Bird" src="../images/bird.jpg" />
     </Layout>
   );
 };
